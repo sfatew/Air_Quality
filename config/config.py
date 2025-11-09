bb08c74d74fad532172f8e0da1cc0d0bf8011f3a
sfatew <130330044+sfatew@users.noreply.github.com>
2025-11-09 16:59:29 +0700

    Refactor GIS config and add AOD/MODIS configurations
    Updated SERVER URL format and added new configuration classes for AOD and MODIS.

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class GIS_config(BaseSettings):    
     GIS_USERNAME: str = ""
     GIS_PASSWORD: str = ""
-    SERVER: str = 'https://arthurhouhttps.pps.eosdis.nasa.gov/text'
+    SERVER: str = 'arthurhouftps.pps.eosdis.nasa.gov'
 
 gis_config = GIS_config()
 
@@ -14,5 +14,12 @@ class Earthdata_config(BaseSettings):
 earthdata_config = Earthdata_config()
 
 class AOD_config(BaseSettings):
-    FTP_USER = ""
-    FTP_PASS = ""
+    FTP_USER: str = ""
+    FTP_PASS: str = ""
+
+aod_config = AOD_config()
+
+class MODIS_config(BaseSettings):
+    TOKEN: str = ""
+
+modis_config = MODIS_config()
